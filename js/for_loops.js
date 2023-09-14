1e73f74771fa0d2e8ec0778b67e9ef2b57225faf
Anna <annasereno27@gmail.com>
2023-09-14 16:26:05 -0500

    completed exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,21 +1,53 @@
 //'use strict';
-/*function showMultiplicationTable(x){
+
+
+//EXERCISE 2
+/* function showMultiplicationTable(x){
 	for (let i = 1; i <= 10; i++) {
-	console.log (`${x} x ${i}`)
-	};
+		console.log (`${x} x ${i}`);
+		console.log (x * i);
+	}
 }
 
 showMultiplicationTable(9)
 */
 
 
-
+//EXERCISE 3
 /*for (let i = 1; i <= 10; i++) {
-	const random = Math.floor((Math.random() * 181) + 20)
-	if (i % 2 !== 0) {
-		continue;
+	const random = Math.floor((Math.random() * 181) + 20);
+		if (random % 2 === 0){
+			console.log (`${random} is even`);
+		}else {
+		console.log (`${random} is odd`);
 	}
-	console.log(random);
 }
 */
+
+
+//EXERCISE 4
+/*function exercise4() {
+	for (let i = 1; i < 10; i++){
+		numberRow(i);
+	}
+}
+function numberRow(numNumbers){
+	// output a row in the console that is that many digits long
+	// for example numberRow(5)
+	// will print 55555
+	for (let i = 0; i < numNumbers; i++) {
+		console.log(output);
+	}
+}
+*/
+
+//EXERCISE 5
+/* increaseBy5(5)
+function increaseBy5(x){
+	for (let i = 1; i <= 20; i++) {
+	console.log (x * i)
+	}
+}
+*/
+
 
