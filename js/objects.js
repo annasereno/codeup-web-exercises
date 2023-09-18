c7f3f8ed5e60c5ab4c3b18a2b69f76bc166bfb14
Anna <annasereno27@gmail.com>
2023-09-18 16:52:27 -0500

    started exercises

--- js/objects.js
+++ js/objects.js
@@ -10,41 +10,51 @@
 	 *  > console.log(person.firstName) // "Rick"
 	 *  > console.log(person.lastName) // "Sanchez"
 	 */
-var person = new Object;
-console.log(person);
+let person = new Object;
 
-firstName = "Rick";
-lastName = "Sanchez";
+person.firstName = "Rick";
+person.lastName = "Sanchez"
+
+console.log (person.firstName + " " + person.lastName)
 
 	/**
 	 * TODO:
 	 * Add a sayHello method to the person object that returns a greeting using
 	 * the firstName and lastName properties.
 	 * console.log the returned message to check your work
-	 *
 	 * Example
 	 * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
 	 */
 
+function sayHello() {
+	return (`Hello from ${person.firstName + " " + person.lastName}`);
+}
+console.log (sayHello());
+
+
 	/** TODO:
-	 * HEB has an offer for the shoppers that buy products amounting to
-	 * more than $200. If a shopper spends more than $200, they get a 12%
-	 * discount. Write a JS program, using conditionals, that logs to the
-	 * browser, how much Ryan, Cameron and George need to pay. We know that
-	 * Cameron bought $180, Ryan $250 and George $320. Your program will have to
-	 * display a line with the name of the person, the amount before the
-	 * discount, the discount, if any, and the amount after the discount.
-	 *
-	 * Uncomment the lines below to create an array of objects where each object
-	 * represents one shopper. Use a foreach loop to iterate through the array,
+	 * HEB has an offer for the shoppers that buy products amounting to more than $200.
+	 * If a shopper spends more than $200, they get a 12% discount.
+	 * Write a JS program, using conditionals, that logs to the browser,
+	 * how much Ryan, Cameron and George need to pay.
+	 * We know that Cameron bought $180, Ryan $250 and George $320.
+	 * Your program will have to display a line with
+	 * the name of the person, the amount before the discount,
+	 * the discount, if any, and the amount after the discount.
+	 * Uncomment the lines below to create an array of objects where each object represents one shopper.
+	 * Use a foreach loop to iterate through the array,
 	 * and console.log the relevant messages for each person
 	 */
+function qualifiesForDiscount(amount){
+	return amount >= 200;
+}
+console.log (qualifiesForDiscount())
 
-	// var shoppers = [
-	//     {name: 'Cameron', amount: 180},
-	//     {name: 'Ryan', amount: 250},
-	//     {name: 'George', amount: 320}
-	// ];
+	var shoppers = [
+	    {name: 'Cameron', amount: 180},
+	    {name: 'Ryan', amount: 250},
+	    {name: 'George', amount: 320}
+	];
 
 	/** TODO:
 	 * Create an array of objects that represent books and store it in a
