66bc9ea8ff57c5b9b42c5b4d0b5bb0388d3a4c72
Anna <annasereno27@gmail.com>
2023-09-21 16:31:45 -0500

    modified conditional.js

--- js/conditionals.js
+++ js/conditionals.js
@@ -21,80 +21,80 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
-/*function analyzeColor(colorName){
-	if (colorName ==='blue'){
-		return "Blue is the color of the sky.";
-	} else if (colorName === 'red'){
-		return "Strawberries are red.";
-	} else if (colorName === 'cyan'){
-		return "I dont know anything about cyan."
-	} else {
-		return "that is an ugly color."
-	}
-}
-*/
+// function analyzeColor(colorName){
+// 	if (colorName ==='blue'){
+// 		return "Blue is the color of the sky.";
+// 	} else if (colorName === 'red'){
+// 		return "Strawberries are red.";
+// 	} else if (colorName === 'cyan'){
+// 		return "I dont know anything about cyan."
+// 	} else {
+// 		return "that is an ugly color."
+// 	}
+// }
+//
+//
+//
+// // Don't change the next two lines!
+// // These lines create two variables for you:
+// // - `colors`: a list of the colors of the rainbow
+// // - `randomColor`: contains a single random color value from the list (this will contain a different color every time the page loads)
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+// const randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+//
+// /**
+//  * TODO:
+//  * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+//  * You should see a different message every time you refresh the page
+//  */
+//
+// console.log(analyzeColor(randomColor))
+//
+//
+//
+// /**
+//  * TODO:
+//  * Comment out the code above, and refactor your function to use a switch-case statement
+//  */
+//  function analyzeColor(colorName) {
+// 	switch (colorName) {
+// 		case ('blue'):
+// 			return  "Blue is the color of the sky.";
+// 		case ('red'):
+// 			return "Strawberries are red.";
+// 		case ('cyan'):
+// 			return "I don'/t know anything about cyan.";
+// 		default:
+// 			return "That is an ugly color.";
+// 	}
+// }
+//
+//
+// /**
+//  * TODO:
+//  * Prompt the user for a color when the page loads, and pass the input from the
+//  * user to your `analyzeColor` function. Alert the return value from your
+//  * function to show it to the user.
+//  */
+//
+// let colorName = prompt("What color do you like?");
+//
+// switch (colorName) {
+// 	case "blue":
+// 		alert("Blue is the color of the sky.");
+// 		break;
+// 	case "red":
+// 		alert("Strawberries are red.");
+// 		break;
+// 	case "cyan":
+// 		alert("I don'/t know anything about cyan.");
+// 		break;
+// 	default:
+// 		alert("That is an ugly color.");
+// 		break;
+// 	}
 
-
-// Don't change the next two lines!
-// These lines create two variables for you:
-// - `colors`: a list of the colors of the rainbow
-// - `randomColor`: contains a single random color value from the list (this will contain a different color every time the page loads)
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-
-
-/**
- * TODO:
- * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
- * You should see a different message every time you refresh the page
- */
-
-//console.log(analyzeColor(randomColor))
-
-
-
-/**
- * TODO:
- * Comment out the code above, and refactor your function to use a switch-case statement
- */
-/* function analyzeColor(colorName) {
-	switch (colorName) {
-		case ('blue'):
-			return  "Blue is the color of the sky.";
-		case ('red'):
-			return "Strawberries are red.";
-		case ('cyan'):
-			return "I don'/t know anything about cyan.";
-		default:
-			return "That is an ugly color.";
-	}
-}
-*/
-
-/**
- * TODO:
- * Prompt the user for a color when the page loads, and pass the input from the
- * user to your `analyzeColor` function. Alert the return value from your
- * function to show it to the user.
- */
-
-/* let colorName = prompt("What color do you like?");
-
-switch (colorName) {
-	case "blue":
-		alert("Blue is the color of the sky.");
-		break;
-	case "red":
-		alert("Strawberries are red.");
-		break;
-	case "cyan":
-		alert("I don'/t know anything about cyan.");
-		break;
-	default:
-		alert("That is an ugly color.");
-		break;
-	}
-*/
 //////OTHER WAY
 /*const userColor = prompt ("Tell me a color");
 const colorSentence = analyzeColor(userColor);
@@ -118,22 +118,24 @@ alert (colorSentence);
  * Test your function by passing it various values and checking for the expected return value.
  */
 
-function calculateTotal (luckyNum, totalPrice){
-	switch (luckyNum){
+function calculateTotal (luckyNumber, total){
+	switch (luckyNumber){
 		case 0:
-			return
+			return total;
 		case 1:
-			return
+			return total * .9;
 		case 2:
-			return
+			return total * .75;
 		case 3:
-			return
+			return total * .65;
 		case 4:
-			return
+			return total * .5;
 		case 5:
-			return
+			return 0;
 	}
 }
+
+
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 5.
@@ -142,11 +144,13 @@ function calculateTotal (luckyNum, totalPrice){
  * and alerts to display to the user what their lucky number was, what their
  * price before the discount was, and what their price after the discount is.
  */
-// Generate a random number between 0 and 6
-//var luckyNumber = Math.floor(Math.random() * 6);
-const luckyNumber = Math.floor(math.random() * 6);
-const userBill = prompt("What was your total bill?")
-alert(`Your lucky number was ${luckyNumber}`)
+//Generate a random number between 0 and 5
+var luckyNumber = Math.floor(Math.random() * 6);
+
+// const userBill = prompt("What was your total bill?")
+// alert(`Your lucky number was ${luckyNumber}`)
+// alert(`Your price before discount was ${userBill}`);
+// alert(`Your price after discount is $${calculateTotal(luckyNumber, userBill)}`);
 /**
  * TODO:
  * Write some JavaScript that uses a `confirm` dialog to ask the user if they
@@ -166,6 +170,35 @@ alert(`Your lucky number was ${luckyNumber}`)
  * HINT: The way we prompt for a value could be improved
  */
 
-///QUESTIONS I HAVE AND KEEP FORGETTING TO ASK///
-//why ` or ' or "
-// why const vs variable?
+if(confirm("Would you like to enter a number?")){
+   const num = Number(prompt("Enter a number:"));
+   if (isNaN(num)){
+       alert("That is not the correct data type.");
+   } else {
+       if (isEven(num)){
+           alert("That is an even number");
+       } else {
+           alert("That is an odd number");
+       }
+
+       alert(`${num} plus 100 equals ${add100(num)}`);
+
+       if (!isPositive(num)){
+           alert("That number is negative.");
+       } else {
+           alert("That number is positive.");
+       }
+   }
+}
+
+	function isEven(number){
+		return number % 2 === 0;
+	}
+
+	function isPositive(number){
+		return number >= 0;
+	}
+
+	function add100(number){
+		return number + 100;
+	}
