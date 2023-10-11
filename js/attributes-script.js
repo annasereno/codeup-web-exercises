3729d286dec7e3fc732770689d8926c74a5b8cce
Anna <annasereno27@gmail.com>
2023-10-11 09:18:49 -0500

    continued exercise

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -1,8 +1,5 @@
-//
-// const newProfilePic = document.querySelector("profile-pic")
-// console.log(newProfilePic.src);
 
-//Two seconds after the page loads, change the "profile-pic" src attribute to a different image.
+//** Two seconds after the page loads, change the "profile-pic" src attribute to a different image.
 function changeProfilePic(){
 	const img =document.querySelector("#profile-pic");
 	img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTesIZWqhm8cBsqUbxrJXFIIPl5mAwUSKuA5wbDBTKcr1oJ5tvMIOL8T0_V7Xyqx3F9Je4&usqp=CAU"
@@ -10,24 +7,35 @@ function changeProfilePic(){
 //changeProfilePic()
 //setInterval(changeProfilePic, 2000);
 
-//Four seconds after the page loads, use innerHTML to change the name in "profile-name".
+
+//** Four seconds after the page loads, use innerHTML to change the name in "profile-name".
 function changeProfileName(){
 	const profileName = document.getElementById("profile-name");
 	profileName.innerHTML = "Stitch"
 }
 //setInterval(changeProfileName,4000);
 
-//Six seconds after page loads, add a new class to "profile-desc" that changes the color and font of the description text.
+
+//** Six seconds after page loads, add a new class to "profile-desc" that changes the color and font of the description text.
 function descriptionText(){
 	const profileDesc = document.querySelector("#profile-desc");
 	profileDesc.classList.add("profile-description");
 }
 // setInterval(descriptionText, 6000);
 
-//Use setTimeout to create these behaviors
+
+//** Use setTimeout to create these behaviors
 function timeOut() {
 	setTimeout(changeProfilePic,2000);
 	setTimeout(changeProfileName,4000);
 	setTimeout(descriptionText,6000);
 }
 timeOut()
+
+
+//** Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval.
+function changeCardBackgroundColor(){
+	profileCardColor = document.getElementById("profile-card");
+	profileCardColor.classList.toggle("card-background")
+}
+setInterval(changeCardBackgroundColor,2000)
