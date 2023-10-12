165e85845d5bec2ac11be482c785dad301221394
Anna <annasereno27@gmail.com>
2023-10-12 16:27:18 -0500

    added button

--- js/parks.js
+++ js/parks.js
@@ -1,2 +1,9 @@
-const button = document.querySelector("button");
+ document.querySelector("button").addEventListener("click", function (){
+		let uls = document.querySelectorAll("ul");
+		uls.forEach(function(ul){
+			let lastLi = ul.lastElementChild;
+			lastLi.style.backgroundColor = "yellow"
+		})
+	});
+
 
